84f3b63ec6164e42ce8dd7249150f9926f1ce366
suwakouhei <s.k.293758@icloud.com>
2021-06-22 18:18:09 +0900

    各々の修正点

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
@@ -23,17 +24,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-      @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -42,8 +40,9 @@ class PrototypesController < ApplicationController
   end
   
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
     
@@ -56,6 +55,10 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def contributor_confirmation
